6615956467eca383161f44226d1b59be77b57e19
Andre P. Bertoletti <apbertoletti@gmail.com>
2015-10-05 17:53:20 -0300

    AULA 8: Novos INSERTs preparatórios

--- BDLoja/S02 - BDLoja - Insere dados de exemplo.sql
+++ BDLoja/S02 - BDLoja - Insere dados de exemplo.sql
@@ -6,6 +6,10 @@
 * SCRIPT: Inserção dos dados de exemplo
 **************************************************************/
 
+-- Selecionando o BDLoja
+USE BDLoja
+
+GO
 
 -- Inserindo dados na tabela de Lojas
 INSERT INTO Loja VALUES ('12345678912345', 'Shopping Central', '3544-1145')
@@ -48,5 +52,45 @@ INSERT INTO Produto VALUES ('Meia para futebol de campo (branca)', 45.00, 5)
 INSERT INTO Produto VALUES ('Meia para futebol de campo (verde)', 45.00, 5)
 INSERT INTO Produto VALUES ('Meia para futebol de campo (azul)', 45.00, 5)
 INSERT INTO Produto VALUES ('Tenis de corrida profissional', 750.00, 1)
+
+
+-- Inserindo dados na tabel ade Venda
+INSERT INTO Venda VALUES (1, '12345678912345', '20150102 10:20')
+INSERT INTO Venda VALUES (2, '12345678912345', '20150102 11:25')
+INSERT INTO Venda VALUES (1, '12345678912345', '20150103 10:00')
+INSERT INTO Venda VALUES (4, '12345678912345', '20150103 11:25')
+INSERT INTO Venda VALUES (3, '12345678912345', '20150103 15:00')
+INSERT INTO Venda VALUES (5, '12345678912345', '20150315 15:00')
+INSERT INTO Venda VALUES (1, '98754124541001', '20150422 14:00')
+INSERT INTO Venda VALUES (4, '98754124541001', '20150423 12:25')
+INSERT INTO Venda VALUES (2, '98754124541001', '20150422 14:00')
+INSERT INTO Venda VALUES (6, '01234568874512', '20150502 20:25')
+
+           
+-- Inserindo dados na tabel ade VendaProduto
+INSERT INTO [VendaProduto] VALUES (1, 3, 200, 10, 2000)
+INSERT INTO [VendaProduto] VALUES (1, 1, 150, 2, 300)
+INSERT INTO [VendaProduto] VALUES (1, 6, 50, 3, 150)
+INSERT INTO [VendaProduto] VALUES (1, 7, 355.30, 1, 355.30)
+INSERT INTO [VendaProduto] VALUES (2, 3, 180, 20, 3600)
+INSERT INTO [VendaProduto] VALUES (3, 1, 150, 2, 300)
+INSERT INTO [VendaProduto] VALUES (4, 11, 45, 1, 45)
+INSERT INTO [VendaProduto] VALUES (5, 7, 355.30, 1, 355.30)
+INSERT INTO [VendaProduto] VALUES (5, 10, 30, 10, 300)
+INSERT INTO [VendaProduto] VALUES (6, 3, 200, 10, 2000)
+INSERT INTO [VendaProduto] VALUES (6, 6, 50, 3, 150)
+INSERT INTO [VendaProduto] VALUES (7, 1, 150, 2, 300)
+INSERT INTO [VendaProduto] VALUES (7, 3, 200, 2, 400)
+INSERT INTO [VendaProduto] VALUES (7, 5, 50, 3, 150)
+INSERT INTO [VendaProduto] VALUES (7, 9, 45, 2, 90)
+INSERT INTO [VendaProduto] VALUES (7, 12, 750, 1, 750)
+INSERT INTO [VendaProduto] VALUES (8, 2, 300, 1, 300)
+INSERT INTO [VendaProduto] VALUES (8, 12, 750, 1, 750)
+INSERT INTO [VendaProduto] VALUES (9, 5, 50, 2, 100)
+INSERT INTO [VendaProduto] VALUES (10, 2, 300, 1, 300)
+INSERT INTO [VendaProduto] VALUES (10, 3, 200, 1, 200)
+
+
+
 
 
